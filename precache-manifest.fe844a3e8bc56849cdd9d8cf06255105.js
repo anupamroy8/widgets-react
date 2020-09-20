f24831b950112a54d7405cf6061126988033f588
anupamroy8 <anupamroy8@gmail.com>
2020-09-20 11:24:34 +0530

    Updates

---
+++ precache-manifest.fe844a3e8bc56849cdd9d8cf06255105.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c7f6f5ddca60840de7d402ecc0586d",
+    "url": "/widget-react/index.html"
+  },
+  {
+    "revision": "ab722bc3bdac58a2c22f",
+    "url": "/widget-react/static/js/2.6292b5f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/widget-react/static/js/2.6292b5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f77ff7507c11be4ffc",
+    "url": "/widget-react/static/js/main.92684281.chunk.js"
+  },
+  {
+    "revision": "245df91b2c44d2f441ed",
+    "url": "/widget-react/static/js/runtime-main.de94ab28.js"
+  }
+]);
